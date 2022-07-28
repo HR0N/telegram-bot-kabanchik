21e9f4c5586bab3374c0d4d2c28d3fee7a79a1e7
HR0N <vivante.des@gmail.com>
2022-07-28 20:27:08 +0300

    init2

---
+++ bot.php
@@ -0,0 +1,19 @@
+<?php
+include('vendor/autoload.php');
+use Telegram\Bot\Api;
+
+$telegram = new Api('5591524736:AAGXk3kxgnGrjpIeMvhMM_toBda5NQVTLnQ');
+$result = $telegram->getWebhookUpdates();
+
+$text = $result['message']['text'];
+$chat_id = $result['message']['chat']['id'];
+$name = $result['message']['from']['username'];
+$first_name = $result['message']['from']['first_name'];
+$last_name = $result['message']['from']['last_name'];
+
+if($text == '/start'){
+    $reply = "Hello world!";
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
+}
+//https://api.telegram.org/bot5591524736:AAGXk3kxgnGrjpIeMvhMM_toBda5NQVTLnQ/setWebHook?url=
+//https://kabanchik-bot.evilcode.space/bot.php
