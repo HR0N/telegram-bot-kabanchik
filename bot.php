9f4c19931a950c2f049623a4905dad74e273b094
HR0N <vivante.des@gmail.com>
2022-07-29 17:16:43 +0300

    ok, tg bot necessarily need administrative rights

--- bot.php
+++ bot.php
@@ -1,19 +1,29 @@
 <?php
-include('vendor/autoload.php');
+require ('vendor/autoload.php');
 use Telegram\Bot\Api;
+include('env.php');
+use env\Env as env;
 
-$telegram = new Api('5591524736:AAGXk3kxgnGrjpIeMvhMM_toBda5NQVTLnQ');
+$telegram = new Api(env::$TELEGRAM_BOT_TOKEN);
 $result = $telegram->getWebhookUpdates();
 
-$text = $result['message']['text'];
+$text = strtolower($result['message']['text']);
 $chat_id = $result['message']['chat']['id'];
 $name = $result['message']['from']['username'];
 $first_name = $result['message']['from']['first_name'];
 $last_name = $result['message']['from']['last_name'];
 
+
 if($text == '/start'){
     $reply = "Hello world!";
     $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
 }
+if($text == 'hello bot'){
+    $reply = "Fuck off bitch!";
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
+}
+
+
+//$ composer require vlucas/phpdotenv
 //https://api.telegram.org/bot5591524736:AAGXk3kxgnGrjpIeMvhMM_toBda5NQVTLnQ/setWebHook?url=
 //https://kabanchik-bot.evilcode.space/bot.php
---
+++ env.example.php
@@ -0,0 +1,15 @@
+<?php
+namespace env;
+
+class Env{
+    public static $TELEGRAM_BOT_TOKEN = 'TOKEN';
+    public static $DB_CONNECTION='mysql';
+    public static $DB_HOST='127.0.0.1';
+    public static $DB_PORT='3306';
+    public static $DB_DATABASE='laravel';
+    public static $DB_USERNAME='root';
+    public static $DB_PASSWORD='';
+}
+
+
+
